d151207fb96ea92a7c11d5492d09fc1d6e3348b6
shino-sh <geassr2.ss@gmail.com>
2018-01-07 17:27:45 +0900

    first commit

---
+++ assets/scripts/game.js
@@ -0,0 +1,92 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        soldierPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+
+        tiledMap: {
+            default: null,
+            type: cc.TiledMap,
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        var manager = cc.director.getPhysicsManager();
+        manager.enabled = true;
+        manager.debugDrawFlags = 
+            // cc.PhysicsManager.DrawBits.e_aabbBit |
+            // cc.PhysicsManager.DrawBits.e_pairBit |
+            cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+            // cc.PhysicsManager.DrawBits.e_jointBit |
+            cc.PhysicsManager.DrawBits.e_shapeBit
+            ;
+        manager.gravity = cc.v2(0, -640);
+
+
+        this.tiledMapOffset = this.tiledMap.node.position;
+        this.tiledMapHeight = this.tiledMap.node.height;
+
+        let objects = this.tiledMap.getObjectGroup("object1").getObjects();
+
+        for(let i = 0; i < objects.length; i++){
+            //
+            // cc.Node
+            //
+            let newNode = new cc.Node();
+            newNode.position = cc.v2(
+                objects[i].offset.x + this.tiledMapOffset.x,
+                this.tiledMapHeight - objects[i].objectSize.height - objects[i].offset.y + this.tiledMapOffset.y
+            );
+            newNode.anchorX = 0;
+            newNode.anchorY = 0;
+            newNode.size = objects[i].objectSize;
+
+            //
+            // cc.RigidBody
+            //
+            let rigidBody = newNode.addComponent(cc.RigidBody);
+            rigidBody.type = cc.RigidBodyType.Static;
+            
+            //
+            // cc.PhysicsBoxCollider
+            //
+            let boxCollider = newNode.addComponent(cc.PhysicsBoxCollider);
+            // boxCollider.body = rigidBody;
+            boxCollider.size = objects[i].objectSize;
+            boxCollider.offset = cc.v2(
+                objects[i].objectSize.width * 0.5,
+                objects[i].objectSize.height * 0.5
+            );
+
+            this.node.addChild(newNode);
+        }
+
+        var newSoldier = cc.instantiate(this.soldierPrefab);
+        // newSoldier.setPosition(cc.p(-350, 100));
+        newSoldier.setPosition(cc.p(0, 100));
+        this.node.addChild(newSoldier);
+    },
+
+    start () {
+
+    },
+
+    update: function (dt) {
+    },
+
+});
---
+++ assets/scripts/soldier.ts
@@ -0,0 +1,111 @@
+// Learn TypeScript:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/typescript/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Soldier extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    distance: int = 30;
+
+    @property
+    delay: int = 1;
+
+    @property(cc.Prefab)
+    shootPrefab: cc.Prefab = null;
+
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.moveRight = false;
+        this.shootFlg = false;
+    },
+
+    start () {
+    	this.setInputControl();
+    },
+
+    update (dt) {
+        var posX = this.node.x;
+        var posY = this.node.y;
+
+        if (this.moveRight) {
+            this.move = cc.moveBy(this.delay, cc.p(this.distance, 0));
+            this.node.runAction(this.move);
+            // posX += 5;
+        }
+        if (this.jumpFlg) {
+            this.jump = cc.moveBy(this.delay, cc.p(0, this.distance * 11));
+            this.node.runAction(this.jump);
+            // posY += 5;
+        }
+        if (this.shootFlg) {
+            this.shoot();
+        }
+
+        // this.node.setPosition(posX, posY);
+        this.moveRight = false;
+        this.shootFlg = false;
+        this.jumpFlg = false;
+    },
+
+
+    //
+
+    shoot () {
+        var newShoot = cc.instantiate(this.shootPrefab);
+        this.node.addChild(newShoot);
+        newShoot.setPosition(cc.p(24, -3));
+    },
+
+    setInputControl () {
+        var self = this;
+        self.move = cc.moveBy(this.delay, cc.p(this.distance, 0));
+        self.jump = cc.moveBy(this.delay, cc.p(0, this.distance));
+        // self.node.runAction(self.move);
+                        
+        cc.eventManager.addListener({       
+            event: cc.EventListener.KEYBOARD,
+        
+            onKeyPressed (keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.q:
+                        self.moveRight = false;
+                        self.shootFlg = false;
+                        self.jumpFlg = true;
+                        // self.node.runAction(self.jump);
+                        break;
+                    case cc.KEY.a:
+                        self.moveRight = true;
+                        self.shootFlg = false;
+                        self.jumpFlg = false;
+                        // self.node.runAction(self.move);
+                        break;
+                    case cc.KEY.s:
+                        self.moveRight = false;
+                        self.shootFlg = true;
+                        self.jumpFlg = false;
+                        break;
+                    default:
+                        self.moveRight = false;
+                        self.shootFlg = false;
+                        self.jumpFlg = false;
+                        break;
+                }
+            }
+        }, this.node)
+    },
+
+}
